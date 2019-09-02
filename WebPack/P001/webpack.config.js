94a6184efdd3e061bff4fd20db380a15c83e4561
Sutirtha Marjit <sutirtha.social.marjit@gmail.com>
2019-09-02 11:54:28 +0530

    Practice: Old webpack removed

--- WebPack/P001/webpack.config.js
+++
@@ -1,7 +0,0 @@
-let WEB_PACK;
-console.log("WebConfig Application started");
-process.env.NODE_ENV = process.env.NODE_ENV || "developemnt";
-WEB_PACK = require('./webpack.dev.js');
-
-
-module.exports = WEB_PACK;
